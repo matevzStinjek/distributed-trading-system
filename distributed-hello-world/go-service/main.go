24f0ce2f4b4fe4dc6252f433309bf4a3881f390d
Matevz Stinjek <stinjek@gmail.com>
2025-03-23 11:32:56 +0100

    go service healthcheck endpoint

--- distributed-hello-world/go-service/main.go
+++ distributed-hello-world/go-service/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("hello")
+	r := mux.NewRouter()
+	r.HandleFunc("/health", HealthHandler)
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	log.Printf("Server starting on :8080")
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server error: %v", err)
+	}
+}
+
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "healthy"})
 }
