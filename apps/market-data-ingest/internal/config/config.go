3835094ba146cd5ad1ecd2588e47ffef2b82df56
Matevz Stinjek <stinjek@gmail.com>
2025-04-22 12:53:30 +0200

    channel configs

--- apps/market-data-ingest/internal/config/config.go
+++ apps/market-data-ingest/internal/config/config.go
@@ -5,10 +5,16 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
+)
+
+const (
+	AGG_INTERVAL_MS_DEFAULT = 50
+	AGG_INTERVAL_MS_MIN     = 10
 )
 
 type Config struct {
-	Symbols []string
+	AggregatorInterval time.Duration
 
 	KafkaBrokers         []string
 	KafkaTopicMarketData string
@@ -22,17 +28,27 @@ type Config struct {
 	RedisPubsubUser string
 	RedisPubsubPw   string
 
-	TradeChannelBuff int
+	Symbols []string
+
+	RawTradesChanBuff  int // 1k-5k
+	ProcTradesChanBuff int // 500-1k
+	KafkaChanBuff      int // 1k-10k
 }
 
 func LoadConfig() (*Config, error) {
+	aggregatorIntervalMs, err := strconv.Atoi(os.Getenv("AGGREGATOR_INTERVAL_MS"))
+	if err != nil || aggregatorIntervalMs < AGG_INTERVAL_MS_MIN {
+		log.Printf("AGGREGATOR_INTERVAL_MS couldn't be parsed to a number or is less than %d, defaulting to %d", AGG_INTERVAL_MS_MIN, AGG_INTERVAL_MS_DEFAULT)
+		aggregatorIntervalMs = AGG_INTERVAL_MS_DEFAULT
+	}
+
 	redisCacheDB, err := strconv.Atoi(os.Getenv("REDIS_CACHE_DB"))
 	if err != nil {
 		log.Fatalf("REDIS_CACHE_DB can't be parsed to a number: %v", err)
 	}
 
 	cfg := &Config{
-		Symbols: []string{"AAPL", "MSFT", "GOOG", "AMZN", "TSLA"},
+		AggregatorInterval: time.Duration(aggregatorIntervalMs) * time.Millisecond,
 
 		KafkaBrokers:         strings.Split(os.Getenv("KAFKA_BROKERS"), ","),
 		KafkaTopicMarketData: os.Getenv("KAFKA_TOPIC_MARKET_DATA"),
@@ -46,7 +62,11 @@ func LoadConfig() (*Config, error) {
 		RedisPubsubUser: os.Getenv("REDIS_PUBSUB_UN"),
 		RedisPubsubPw:   os.Getenv("REDIS_PUBSUB_PW"),
 
-		TradeChannelBuff: 100,
+		Symbols: []string{"AAPL", "MSFT", "GOOG", "AMZN", "TSLA"},
+
+		RawTradesChanBuff:  100,
+		ProcTradesChanBuff: 50,
+		KafkaChanBuff:      500,
 	}
 	return cfg, nil
 }
